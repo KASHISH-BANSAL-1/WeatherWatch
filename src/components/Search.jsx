a0747c620f14a42fc08758795f371e707a03ffc4
KASHISH-BANSAL-1 <kashish.bansal_cs21@gla.ac.in>
2024-09-15 20:51:47 +0530

    history

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../style/search.css';
 
@@ -6,8 +6,30 @@ const Search = () => {
   const [city, setCity] = useState('');
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
+  const [recentSearches, setRecentSearches] = useState([]);
+  const [showHistory, setShowHistory] = useState(false);
   const navigate = useNavigate();
   const apiKey = '23cec2d01085dedef5de9178b9e3fbdd';
+
+  useEffect(() => {
+    const storedSearches = JSON.parse(localStorage.getItem('recentSearches')) || [];
+    setRecentSearches(storedSearches);
+  }, []);
+
+  const storeRecentSearch = (city) => {
+    let searches = [...recentSearches];
+    
+    if (!searches.includes(city)) {
+      searches.unshift(city);
+      
+      if (searches.length > 5) {
+        searches.pop();
+      }
+      
+      setRecentSearches(searches);
+      localStorage.setItem('recentSearches', JSON.stringify(searches));
+    }
+  };
 
   const fetchWeatherData = async () => {
     setLoading(true);
@@ -22,6 +44,9 @@ const Search = () => {
 
       const data = await response.json();
       setError('');
+      
+      storeRecentSearch(city);
+
       navigate('/weather', { state: { data } });
     } catch (err) {
       setError(err.message);
@@ -35,6 +60,10 @@ const Search = () => {
     if (city) {
       fetchWeatherData();
     }
+  };
+
+  const toggleHistory = () => {
+    setShowHistory(!showHistory);
   };
 
   return (
@@ -53,6 +82,22 @@ const Search = () => {
           </button>
         </form>
         {error && <p className="error">{error}</p>}
+
+        <button className="history-button" onClick={toggleHistory}>
+          {showHistory ? 'Hide History' : 'Show History'}
+        </button>
+
+        {showHistory && (
+          <div className="recent-searches">
+            <ul>
+              {recentSearches.map((search, index) => (
+                <li className='list' key={index} onClick={() => setCity(search)}>
+                  {search}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
